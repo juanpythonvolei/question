3f1eeef3258bcafb6bd360fe72f8875017fe1c11
Juan Pablo <juanzsalca@outlook.com>
2025-01-15 12:24:57 -0300

    Update asssistente.py

--- asssistente.py
+++ asssistente.py
@@ -3,18 +3,32 @@ import streamlit as st
 import tempfile
 import os
 import pandas as pd
-
+import speech_recognition as sr
 key = os.getenv('api')
-genai.configure(api_key=key)
+genai.configure(api_key='AIzaSyDxK2kxeqioFC2plNoPVdGUlAM0RHXyURs')
 
+@st.dialog("Atenção")
+def alert():
+     st.text("Você deve realizar uma pergunta")
+
+def audio(audio_value):
+    rec = sr.Recognizer()
+    with sr.AudioFile(audio_value) as arquivo_audio:
+                    audio = rec.record(arquivo_audio)
+                    texto = rec.recognize_google(audio,language ='pt-BR ')
+    return texto
+
+def delete_temp_file(file):
+    if os.path.exists(file): 
+        os.remove(file)
 
 def other_files_jpg(file,pergunta):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
         tmp_file.write(file.read())
-    
     model = genai.GenerativeModel('gemini-1.5-flash')
     chat = model.start_chat(history=[{"role": "user", "parts": [genai.upload_file(tmp_file.name)]}])
     response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
+    delete_temp_file(tmp_file.name)
     return response.text
 
 def other_files_csv(file,pergunta):
