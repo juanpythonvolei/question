54a3a05bf79d329e5b6aba2c27a8711038222f75
Juan Pablo <juanzsalca@outlook.com>
2025-01-15 13:57:27 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -20,45 +20,65 @@ if carregar_arquivo:
             pergunta = audio(audio_value=audio_value)
     if uploaded_file:
         if '.pdf' in uploaded_file.name:
-            conteudo = create_temporary_file(file=uploaded_file)
+            conteudo = create_temporary_file(tipo='.pdf',file=uploaded_file)
             botao_perguntar = st.button("Perguntar (PDF)")
-            if botao_perguntar and pergunta != '':
-                response = ia(pergunta=pergunta, conteudo=conteudo)
-                st.info(response)
-                botao_download  = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
-                delete_temp_file(conteudo)
-                if botao_download:
-                    delete_temp_file(f"{pergunta}.tmp")
+            if botao_perguntar:
+                if pergunta != '':
+                    st.divider()
+                    with st.status("Obtendo resposta",expanded=True,state='running') as status:
+                            response = ia(pergunta=pergunta, conteudo=conteudo)
+                            status.update(label="Resposta obtida",state='complete')
+                    st.info(response)
+                    botao_download  = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                    delete_temp_file(conteudo)
+                    if botao_download:
+                        delete_temp_file(f"{pergunta}.tmp")
+                else:
+                    alert()
         elif '.xlsx' in uploaded_file.name:
-            content = create_temporary_file(file=uploaded_file)
+            content = create_temporary_file(tipo='.xlsx',file=uploaded_file)
             button = st.button("Perguntar (EXCEL)")
-            if button and pergunta != '':
-                response = other_files_excel(file=content,pergunta=pergunta)
-                st.info(response)
-                botao_download = st.download_button("Faça o download da Resposta",response,f'{pergunta}')
-                delete_temp_file(content)
-                if botao_download:
-                    delete_temp_file(f"{pergunta}.tmp")
+            if button:
+                if pergunta != '':
+                    st.divider()
+                    with st.status("Obtendo resposta",expanded=True,state='running') as status:
+                            response = other_files_excel(pergunta=pergunta, file=content)
+                            status.update(label="Resposta obtida",state='complete')
+                    st.info(response)
+                    botao_download = st.download_button("Faça o download da Resposta",response,f'{pergunta}')
+                    delete_temp_file(content)
+                    if botao_download:
+                        delete_temp_file(f"{pergunta}.tmp")
+                else:
+                    alert()
         elif '.csv' in uploaded_file.name:
-            content = create_temporary_file(file=uploaded_file)
+            content = create_temporary_file(tipo='.csv',file=uploaded_file)
             button = st.button("Perguntar (CSV)")
-            if button and pergunta != '':
-                response = other_files_csv(file=content,pergunta=pergunta)
-                st.info(response)
-                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
-                delete_temp_file(content)
-                if botao_download:
-                    delete_temp_file(f"{pergunta}.tmp")
-            else:
-                alert()
+            if button:
+                if pergunta != '':
+                    st.divider()
+                    with st.status("Obtendo resposta",expanded=True,state='running') as status:
+                            response = other_files_csv(pergunta=pergunta, file=content)
+                            status.update(label="Resposta obtida",state='complete')
+                    st.info(response)
+                    botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                    delete_temp_file(content)
+                    if botao_download:
+                        delete_temp_file(f"{pergunta}.tmp")
+                else:
+                    alert()
         elif '.jpeg' in uploaded_file.name or '.jpg' in uploaded_file.name:
             button = st.button("Perguntar (Imagem)")
-            if button and pergunta != '':
-                response = other_files_jpg(file=uploaded_file,pergunta=pergunta)
-                st.info(response)
-                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
-                if botao_download:
-                    delete_temp_file(f'{pergunta}.tmp')
+            if button:
+                if pergunta != '':
+                    st.divider()
+                    response = other_files_jpg(file=uploaded_file,pergunta=pergunta)
+                    st.info(response)
+                    botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                    if botao_download:
+                        delete_temp_file(f'{pergunta}.tmp')
+                else:
+                    alert()
 
 elif tirar_foto:
     foto = st.camera_input("Tire aqui sua foto e a envie para a ia")
@@ -70,15 +90,19 @@ elif tirar_foto:
         with col6:
             audio_value_foto = st.audio_input(label="")
             if audio_value_foto:
-                 pergunta = audio(audio_value=audio_value_foto)
+                 pergunta_da_foto = audio(audio_value=audio_value_foto)
         if pergunta_da_foto:
             botao_foto= st.button("Fazer pergunta da foto")
             if botao_foto:
-                response = other_files_jpg(file=foto,pergunta=pergunta_da_foto)
-                st.info(response)
-                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta_da_foto}")
-                if botao_download:
-                    delete_temp_file(f'{pergunta_da_foto}.tmp')
+                if pergunta_da_foto != '':
+                    st.divider()
+                    response = other_files_jpg(file=foto,pergunta=pergunta_da_foto)
+                    st.info(response)
+                    botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta_da_foto}")
+                    if botao_download:
+                        delete_temp_file(f'{pergunta_da_foto}.tmp')
+                else:
+                    alert()
             
 
         
