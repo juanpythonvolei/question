c16cc5a3998fdb3a381db43fbabb579b40ddc992
Juan Pablo <juanzsalca@outlook.com>
2025-01-14 22:26:06 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -19,27 +19,39 @@ if carregar_arquivo:
             if botao_perguntar:
                 response = ia(pergunta=pergunta, conteudo=conteudo)
                 st.info(response)
-                st.download_button("Faça o download da Resposta",response)
+                botao_download  = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                delete_temp_file(conteudo)
+                if botao_download:
+                    delete_temp_file(f"{pergunta}.tmp")
         elif '.xlsx' in uploaded_file.name:
             content = create_temporary_file(file=uploaded_file)
             button = st.button("Perguntar (EXCEL)")
             if button:
                 response = other_files_excel(file=content,pergunta=pergunta)
                 st.info(response)
-                st.download_button("Faça o download da Resposta",response)
+                botao_download = st.download_button("Faça o download da Resposta",response,f'{pergunta}')
+                delete_temp_file(content)
+                if botao_download:
+                    delete_temp_file(f"{pergunta}.tmp")
         elif '.csv' in uploaded_file.name:
             content = create_temporary_file(file=uploaded_file)
             button = st.button("Perguntar (CSV)")
             if button:
                 response = other_files_csv(file=content,pergunta=pergunta)
                 st.info(response)
-                st.download_button("Faça o download da Resposta",response)
+                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                delete_temp_file(content)
+                if botao_download:
+                    delete_temp_file(f"{pergunta}.tmp")
         elif '.jpeg' in uploaded_file.name or '.jpg' in uploaded_file.name:
             button = st.button("Perguntar (Imagem)")
             if button:
                 response = other_files_jpg(file=uploaded_file,pergunta=pergunta)
                 st.info(response)
-                st.download_button("Faça o download da Resposta",response)
+                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta}")
+                if botao_download:
+                    delete_temp_file(f'{pergunta}.tmp')
+
 elif tirar_foto:
     foto = st.camera_input("Tire aqui sua foto e a envie para a ia")
     if foto:
@@ -49,5 +61,7 @@ elif tirar_foto:
             if botao_foto:
                 response = other_files_jpg(file=foto,pergunta=pergunta_da_foto)
                 st.info(response)
-                st.download_button("Faça o download da Resposta",response)
+                botao_download = st.download_button("Faça o download da Resposta",response,f"{pergunta_da_foto}")
+                if botao_download:
+                    delete_temp_file(f'{pergunta_da_foto}.tmp')
             
