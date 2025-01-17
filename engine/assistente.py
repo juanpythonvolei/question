335663dd6ee236fd8d84635897e25235a5c6ee9d
Juan Pablo <juanzsalca@outlook.com>
2025-01-17 19:51:39 -0300

    Update assistente.py

--- engine/assistente.py
+++ engine/assistente.py
@@ -6,12 +6,58 @@ import pandas as pd
 import speech_recognition as sr
 key = os.getenv('api')
 genai.configure(api_key=key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+def multiple_files(pergunta,files):
+        uploaded_parts = []
+        final = ''
+        for file in files:
+            if '.pdf' in file.name:
+                tempfile = create_temporary_file(tipo='.pdf',file=file)
+                arquivo_carregado = genai.upload_file(tempfile)
+                uploaded_parts.append(arquivo_carregado)
+                delete_temp_file(tempfile)
+            elif '.xlsx' in file.name or '.csv' in file.name :   
+                    arquivo = str(file.name).split('.')
+                    tempfile = create_temporary_file(tipo=f'.{arquivo[1]}',file=file)
+                    try:
+                        data  = pd.read_excel(tempfile)
+                    except:
+                          data = pd.read_csv(tempfile)
+                    texto = data.to_string()
+                    final += f'''Dados do arquivo {tempfile}:\n
+                    {texto}
+                    \n'''
+                    delete_temp_file(tempfile)
+            elif '.jpg' in file.name or '.jpeg' in file.name:
+                    arquivo = str(file.name).split('.')
+                    tempfile = create_temporary_file(tipo=f'.{arquivo[1]}',file=file)
+                    arquivo_carregado = genai.upload_file(tempfile)
+                    uploaded_parts.append(arquivo_carregado)
+                    delete_temp_file(tempfile)
+            elif '.docx' in file.name:
+                    arquivo = str(file.name).split('.')
+                    tempfile = create_temporary_file(tipo=f'.{arquivo[1]}',file=file)
+                    doc = Document(tempfile)
+                    for paragraph in doc.paragraphs:
+                        final += f'{paragraph.text}\n'
+                    delete_temp_file(tempfile)
+            else:
+                    arquivo = str(file.name).split('.')
+                    tempfile = create_temporary_file(tipo=f'.{arquivo[1]}',file=file)
+                    with open(tempfile,'rb') as tmp:
+                        txt = tmp.read()
+                    final += str(f'''Conteúdo código {tempfile}:\n
+                                     {txt}
+\n''')
+                    delete_temp_file(tempfile)
+        uploaded_parts.append({'text':final})
+        model = genai.GenerativeModel('gemini-1.5-flash') 
+        chat = model.start_chat(history=[{"role":"user","parts":uploaded_parts}]) 
+        response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
+        return response.text
+        
 
-def store_video(file):
-    with tempfile.NamedTemporaryFile(dir=r'./videos',delete=False, suffix='.mp4') as tmp_file:
-        tmp_file.write(file.read())
-        return tmp_file.name
-
+              
 @st.dialog("Atenção")
 def alert():
      st.text("Você deve realizar uma pergunta")
@@ -27,21 +73,14 @@ def delete_temp_file(file):
     if os.path.exists(file): 
         os.remove(file)
 
-def other_files_video(file,pergunta):
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmp_file:
-        tmp_file.write(file.read())
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    chat = model.start_chat(history=[{"role": "user", "parts": [genai.upload_file(tmp_file.name)]}])
-    response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
-    delete_temp_file(tmp_file.name)
-    return response.text
-
 def other_files_jpg(file,pergunta):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as tmp_file:
         tmp_file.write(file.read())
     model = genai.GenerativeModel('gemini-1.5-flash')
     chat = model.start_chat(history=[{"role": "user", "parts": [genai.upload_file(tmp_file.name)]}])
-    response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
+    with st.status("Obtendo resposta",expanded=True,state='running') as status:
+                            response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
+                            status.update(label="Resposta obtida",state='complete')
     delete_temp_file(tmp_file.name)
     return response.text
 
@@ -62,9 +101,11 @@ def other_files_excel(file,pergunta):
     return response.text
 
 
-def ia(pergunta,conteudo): 
+def ia(pergunta,conteudo,mime=None):
+    if '.jpg' in conteudo or '.jpeg' in conteudo:
+          mime = 'video/mp4' 
     model = genai.GenerativeModel('gemini-1.5-flash') 
-    chat = model.start_chat(history=[{"role":"user","parts":[genai.upload_file(conteudo)]}]) 
+    chat = model.start_chat(history=[{"role":"user","parts":[genai.upload_file(conteudo,mime_type=mime)]}]) 
     response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}') 
     return response.text
 
@@ -72,3 +113,14 @@ def create_temporary_file(tipo,file):
   with tempfile.NamedTemporaryFile(delete=False, suffix=tipo) as tmp_file:
         tmp_file.write(file.read())
         return tmp_file.name
+  
+def read_word(pergunta,conteudo):
+    texto = ''
+    doc = Document(conteudo)
+    for paragraph in doc.paragraphs:
+          texto += f'{paragraph.text}\n'
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    chat = model.start_chat(history=[{"role": "user", "parts": [{"text": texto}]}])
+    response = chat.send_message(f'Você é uma analista e sua função é reponder as peguntas se baseando nas informações que você está recebendo. Assim sendo responda a essa pergunta:{pergunta}')
+    delete_temp_file(conteudo)
+    return response.text
